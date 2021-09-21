8e5915913e13c653c040a3f0f24c847c9dad6af9
Cat <armintas.pakenis@mif.stud.vu.lt>
2021-09-21 16:50:14 +0300

    Parser v0.2

--- src/Parser.hs
+++ src/Parser.hs
@@ -15,7 +15,7 @@ data Surrounding = Surrounding {
                 gates :: [[Int]],
                 ghosts :: [[Int]],
                 wall :: [[Int]]}
-                deriving (Show, Read)
+                deriving (Show, Rd)
 
 -- | TODO: find a way to change 'drop 15' to a more reliable parsing method
 getSurrounding :: String -> Surrounding
