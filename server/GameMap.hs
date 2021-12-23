fc6f31563afb3c606575854713b3abda7e03655e
ArmintasP <empty@noreply.com>
2021-12-23 13:17:38 +0200

    Implement game map module

---
+++ server/GameMap.hs
@@ -0,0 +1,180 @@
+module GameMap where
+import Data.Either as E (Either (..))
+
+newtype GameMap = GameMap [String]
+type Point = (Int, Int)
+type Coordinates = [Point]
+
+data Direction = Right | Left | Up | Down
+  deriving (Show, Eq)
+
+data GameData = GameData {
+        bombermans :: Coordinates,
+        ghosts :: Coordinates,
+        bomb :: Coordinates,
+        bricks :: Coordinates,
+        gates :: Coordinates,
+        wall :: Coordinates,
+        mapSize :: (Int, Int)
+} deriving (Show)
+
+gameDataEmpty :: GameData
+gameDataEmpty = GameData [] [] [] [] [] [] (0, 0)
+
+
+
+moveBomberman :: Direction -> GameData -> GameData
+moveBomberman direction gd
+  | direction == GameMap.Left = moveBomberman' (x - 1, y) gd
+  | direction == GameMap.Right  = moveBomberman' (x + 1, y) gd
+  | direction == GameMap.Up = moveBomberman' (x, y - 1) gd
+  | direction == GameMap.Down = moveBomberman' (x, y + 1) gd
+  | otherwise = gd
+  where
+    [(x, y)] = bombermans gd
+
+--   |
+--  —b—  Bomb's explosion spans to each direction (up, down, left and right) with radius of 1.
+--   |
+explodeBomb :: GameData -> GameData
+explodeBomb gd = gd {bricks = remainingBricks, ghosts = remainingGhosts}
+  where
+    [(x, y)] = bomb gd
+    blastSpots = [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    remainingBricks = removePoints (bricks gd) blastSpots
+    remainingGhosts = removePoints (ghosts gd) blastSpots
+
+plantBomb :: GameData -> GameData
+plantBomb gd = if isBombPlanted then gd  else gd {bomb = plantSpot}
+  where
+    plantSpot = bombermans gd
+    bomb' = bomb gd
+    isBombPlanted = length bomb' == 1
+
+-- | If there is a bomb (x, y), return GameData with elements that are near it (blastSpots). Otherwise return empty GameData.
+getBombSurrounding :: GameData -> GameData
+getBombSurrounding = error "Not implemented"
+
+getBombStatus :: GameData -> GameData
+getBombStatus gd = gameDataEmpty {bomb = bomb gd}
+
+-- | Bomberman's vision range
+radius :: Int
+radius = 5
+
+getSurrounding :: GameData -> GameData
+getSurrounding gd = gd {bombermans = [bm], ghosts = gh, bomb = [], bricks = br, gates = gt, wall = wl}
+  where
+    [bm] = bombermans gd
+    br = getInRange radius bm $ bricks gd
+    gh = getInRange radius bm $ ghosts gd
+    gt = getInRange radius bm $ gates gd
+    wl = getInRange radius bm $ wall gd
+
+-- | Removes points that matches the blacklist.
+removePoints :: [Point] -> [Point] -> [Point]
+removePoints points blacklist = [p | p <- points, p `notElem` blacklist]
+
+getGameMapData :: GameMap -> GameData
+getGameMapData x = constructGameData gd gameMapElements
+  where
+    gameMapElements = getGameMapData' x
+    gd = gameDataEmpty {mapSize = getGameMapSize x}
+
+
+getGameMapSize :: GameMap -> (Int, Int)
+getGameMapSize x = (getGameMapWidth x, getGameMapHeight x)
+
+getGameMapWidth :: GameMap -> Int
+getGameMapWidth (GameMap gameMap) = length $ head gameMap
+
+getGameMapHeight :: GameMap -> Int
+getGameMapHeight (GameMap gameMap) = length gameMap
+
+gameMapCollection :: [GameMap]
+gameMapCollection = [gameMap1, gameMap2]
+
+gameMap1 :: GameMap
+gameMap1 = GameMap [
+        "XXXXXXXXXXXXXXX",
+        "XM    BBBBBBBBX",
+        "XBXBX X X X X X",
+        "X   B B   B   X",
+        "X X X X X XBXBX",
+        "X   B   B     X",
+        "X X XBXBX XBXBX",
+        "X         B B X",
+        "XBXBX XBXBXBXBX",
+        "X     BG      X",
+        "XBXBX XBXBXBX X",
+        "X     B       X",
+        "X XBXBXBXBXBXBX",
+        "X            OX",
+        "XXXXXXXXXXXXXXX"];
+
+gameMap2 :: GameMap
+gameMap2 = GameMap [
+        "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "XM    BBBBBBBBX                                           OX",
+        "XBXBX X X X X X                                            X",
+        "X   B B       X                                            X",
+        "X X X X X XBXBX                                            X",
+        "X   B   B     X                                            X",
+        "X X XBXBX XBXBX          BBBBBBBBBBBBBB                    X",
+        "X         B B X          B      G     B                    X",
+        "XBXBX XBXBXBXBX          BBBBBBBBBBBBBB                    X",
+        "X             X                                            X",
+        "XBXBXBXBXBXBX X                                            X",
+        "X             X                                            X",
+        "X XBXBXBXBXBXBX                                            X",
+        "X                                                          X",
+        "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"];
+
+bombermansSym = 'M'
+bricksSym = 'B'
+gatesSym = 'O'
+ghostsSym = 'G'
+wallSym = 'X'
+bombsSym = 'b'
+
+-- |
+-- | Helper functions
+-- |
+
+
+moveBomberman' :: Point -> GameData -> GameData
+moveBomberman' (x, y) gd = if isOutOfMargins || isBlocked then gd else gd {bombermans = [(x, y)]}
+  where
+    (maxX, maxY) = mapSize gd
+    isOutOfMargins = x < 0 || y < 0 || x >= maxX || y >= maxY
+    isBlocked = ((x, y) `elem` bricks gd) || ((x, y) `elem` wall gd)
+
+
+getInRange :: Int -> Point -> Coordinates -> Coordinates
+getInRange r p [] = []
+getInRange r (x', y') [(x,y)]
+  | (x' - r <= x) && (x' + r >= x) && (y' - r <= x) && (y' + r >= y) = [(x, y)]
+  | otherwise = []
+getInRange r p (x:xs) = getInRange r p [x] ++ getInRange r p xs
+
+
+constructGameData :: GameData -> [(Char, Point)] -> GameData
+constructGameData gd [] = gd
+constructGameData gd [(ch, point)] = addPoint ch point gd
+constructGameData gd ((ch, point):xs) = constructGameData (addPoint ch point gd) xs
+
+getGameMapData' :: GameMap -> [(Char, Point)]
+getGameMapData' (GameMap m) = do
+  (str, y) <- zip m [0..]
+  (sym, x) <- zip str [0..]
+  return (sym, (x, y))
+
+
+addPoint :: Char -> Point -> GameData -> GameData
+addPoint c cord gd
+  | c == bombermansSym = gd {bombermans = bombermans gd ++[cord]}
+  | c == ghostsSym = gd {ghosts = ghosts gd ++ [cord]}
+  | c == bricksSym = gd {bricks = bricks gd ++ [cord]}
+  | c == gatesSym = gd {gates = gates gd ++ [cord]}
+  | c == wallSym = gd {wall = wall gd ++ [cord]}
+  | otherwise = gd
